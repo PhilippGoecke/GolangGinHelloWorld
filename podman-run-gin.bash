83c0a38665ea1ef2573514d3210159a186e815c7
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2025-06-22 00:22:34 +0200

    Update podman-run-gin.bash

--- podman-run-gin.bash
+++ podman-run-gin.bash
@@ -1,3 +1,3 @@
 podman build --no-cache --rm -f Containerfile -t gin:demo .
 podman run --interactive --tty -p 8080:8080 gin:demo
-echo "browse http://localhost:8080/hello"
+echo "browse http://localhost:8080/hello?name=test"
